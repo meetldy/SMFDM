41205b06f1cd3432e2146d518ea530dce95a63f2
meetldy <bao_na2024@126.com>
2024-12-25 10:39:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 # train
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12392 --use_env idm_main.py -p train -c config/ffhq_liifsr3_scaler_16_128.json -r idm_checkpoints/face --sparse --density 0.15
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12392 --use_env idm_main.py -p train -c config/ffhq_liifsr3_scaler_16_128.json -r checkpoints/face --sparse --density 0.15
 
 # val
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=12392 --use_env idm_main.py -p val -c config/ffhq_liifsr3_scaler_16_128.json -r idm_checkpoints/24.11
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=12392 --use_env idm_main.py -p val -c config/ffhq_liifsr3_scaler_16_128.json -r checkpoints/24.11
